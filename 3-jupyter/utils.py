a056144ae1b2dcaedf3148e1e0bc37923cbd1e4a
fimrie <fergus.imrie@keble.ox.ac.uk>
2019-07-10 10:26:14 +0100

    updated jupyter intro

--- 3-jupyter/utils.py
+++ 3-jupyter/utils.py
@@ -1,2 +1,26 @@
-def foo():
-    return "bar"
+import numpy as np
+import pandas as pd
+
+def make_dummy_dataset(sample_size=20, seed=42):
+    """Creates a dummy dataset.
+    
+    Args:
+        sample_size (int, optional): Number of samples in dataset
+        seed (int, optional): Seed for random number generator
+        
+    Returns:
+        dataset (pandas.DataFrame object): A dataframe containing the dataset.
+    """
+    
+    np.random.seed(seed)
+
+    heights = np.random.normal(1.70, 0.1, sample_size).astype(np.float32)
+    weights = np.random.normal(75, 10, sample_size)
+    ages = np.random.uniform(18, 60, sample_size)
+
+    eye_colours = np.array(["Blue", "Brown", "Green", "Other"])
+    eyes = np.random.choice(eye_colours, sample_size)
+    
+    dataset = pd.DataFrame({"Height": heights, "Weight": weights, "Age": ages, "Eye colour": eyes})
+    
+    return dataset
